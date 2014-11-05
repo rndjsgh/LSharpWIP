cedce60c5d682fe053e76e37b55fb55e4c20d237
iJavai <iJava.i@hotmail.co.uk>
2014-11-05 20:54:53 +0000

    dsadafaf

--- ZedSharp/ZedSharp.cs
+++ ZedSharp/ZedSharp.cs
@@ -131,6 +131,10 @@ namespace ZedSharp {
                     else
                         Zed.shadowW = min;
                 }
+
+                if (sender.Name == "Zed_Base_R_buf_tell.troy" && sender.IsEnemy) {
+                    //TODO check if this works it means the enemy is killable with ult and you can then leave him and return to ult shadow if it is still active.
+                }
             }
 
             var spell = (Obj_SpellMissile) sender;
