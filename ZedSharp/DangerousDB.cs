1c70436d4d5da249d3e8242d895dfa349fbcc1bd
DZ191 <darkprince97@live.it>
2014-11-08 20:40:32 +0100

    Another

--- ZedSharp/DangerousDB.cs
+++ ZedSharp/DangerousDB.cs
@@ -51,7 +51,13 @@ namespace ZedSharp
                     }
                 }
             }
-            return false;
+            var dangerLevel = 0;
+            foreach (var DH in DangerousheroNear)
+            {
+                dangerLevel += DH.dangerLevel;
+            }
+            
+            return dangerLevel >= 15;
         }
         bool spellUp(String Champion,SpellSlot slot)
         {
