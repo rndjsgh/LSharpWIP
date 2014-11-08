0cf0627603f0dee2f8d03e38d101addd8db8ecee
iJavai <iJava.i@hotmail.co.uk>
2014-11-08 19:54:42 +0000

    naming is sht

--- ZedSharp/DangerousDB.cs
+++ ZedSharp/DangerousDB.cs
@@ -6,60 +6,70 @@ using LeagueSharp.Common;
 
 namespace ZedSharp {
     internal class DangerousDB {
-        private readonly List<DangerousSpell> dangerousDB = new List<DangerousSpell>();
+        private readonly List<DangerousSpell> dangerousSpells = new List<DangerousSpell>();
 
-        private void compileDB() {
-            dangerousDB.Add(new DangerousSpell {ChampName = "Amumu", spell = SpellSlot.R});
-            dangerousDB.Add(new DangerousSpell
+        private void populateList() {
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Amumu", spell = SpellSlot.R});
+            dangerousSpells.Add(new DangerousSpell
             {ChampName = "Annie", spell = SpellSlot.R, buff = "Pyromania", danger = 5}); //Check buff name
-            dangerousDB.Add(new DangerousSpell
+            dangerousSpells.Add(new DangerousSpell
             {ChampName = "Annie", spell = SpellSlot.Q, buff = "Pyromania", danger = 5}); //Check buff name
-            dangerousDB.Add(new DangerousSpell {ChampName = "Ashe", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Cassiopeia", spell = SpellSlot.R, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Galio", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Gnar", spell = SpellSlot.R, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Gragas", spell = SpellSlot.R, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Leona", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Leona", spell = SpellSlot.Q, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Malphite", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Orianna", spell = SpellSlot.R, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Sona", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Taric", spell = SpellSlot.E, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Alistar", spell = SpellSlot.Q, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Lissandra", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Malzahar", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Riven", spell = SpellSlot.W, danger = 4});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Pantheon", spell = SpellSlot.W, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Sejuani", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell {ChampName = "Skarner", spell = SpellSlot.R, danger = 5});
-            dangerousDB.Add(new DangerousSpell
-            {ChampName = "Fiddlesticks", spell = SpellSlot.Q, danger = getDangerLevelSkill("Fiddlesticks", SpellSlot.Q)});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Ashe", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Cassiopeia", spell = SpellSlot.R, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Galio", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Gnar", spell = SpellSlot.R, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Gragas", spell = SpellSlot.R, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Leona", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Leona", spell = SpellSlot.Q, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Malphite", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Orianna", spell = SpellSlot.R, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Sona", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Taric", spell = SpellSlot.E, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Alistar", spell = SpellSlot.Q, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Lissandra", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Malzahar", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Riven", spell = SpellSlot.W, danger = 4});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Pantheon", spell = SpellSlot.W, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Sejuani", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell {ChampName = "Skarner", spell = SpellSlot.R, danger = 5});
+            dangerousSpells.Add(new DangerousSpell
+            {ChampName = "Fiddlesticks", spell = SpellSlot.Q, danger = getDangerLevel("Fiddlesticks", SpellSlot.Q)});
         }
 
-        private bool canGoIn(Obj_AI_Hero target) {
-            var DangerousheroNear = new List<DangerousHero>();
-            foreach (Obj_AI_Hero enemy in ObjectManager.Get<Obj_AI_Hero>().Where(e => e.IsEnemy)) {
-                foreach (DangerousSpell DP in dangerousDB) {
-                    if (DP.ChampName == enemy.ChampionName && spellUp(enemy.ChampionName, DP.spell) &&
-                        enemy.Distance(target) <= enemy.Spellbook.GetSpell(DP.spell).SData.CastRange[0]) {
-                        DangerousheroNear.Add(new DangerousHero {dangerLevel = DP.danger, hero = enemy, slot = DP.spell});
+        private bool canGoHam(Obj_AI_Hero target) {
+            List<DangerousHero> DangerousheroNear =
+                (from enemy in ObjectManager.Get<Obj_AI_Hero>().Where(e => e.IsEnemy)
+                    from dangerousSpell in dangerousSpells
+                    where
+                        dangerousSpell.ChampName == enemy.ChampionName &&
+                        isSpellReady(enemy.ChampionName, dangerousSpell.spell) &&
+                        enemy.Distance(target) <= enemy.Spellbook.GetSpell(dangerousSpell.spell).SData.CastRange[0]
+                    select
+                        new DangerousHero
+                        {dangerLevel = dangerousSpell.danger, hero = enemy, slot = dangerousSpell.spell}).ToList();
+
+            /**
+             * foreach (Obj_AI_Hero enemy in ObjectManager.Get<Obj_AI_Hero>().Where(e => e.IsEnemy)) {
+                foreach (DangerousSpell dangerousSpell in dangerousSpells) {
+                    if (dangerousSpell.ChampName == enemy.ChampionName &&
+                        isSpellReady(enemy.ChampionName, dangerousSpell.spell) &&
+                        enemy.Distance(target) <= enemy.Spellbook.GetSpell(dangerousSpell.spell).SData.CastRange[0]) {
+                        DangerousheroNear.Add(new DangerousHero
+                        {dangerLevel = dangerousSpell.danger, hero = enemy, slot = dangerousSpell.spell});
                     }
                 }
-            }
-            var dangerLevel = 0;
-            foreach (var DH in DangerousheroNear)
-            {
-                dangerLevel += DH.dangerLevel;
-            }
-            
+            }*/
+
+            int dangerLevel = DangerousheroNear.Sum(dangerousHero => dangerousHero.dangerLevel);
+
             return dangerLevel >= 15;
         }
 
-        private bool spellUp(String Champion, SpellSlot slot) {
+        private bool isSpellReady(String Champion, SpellSlot slot) {
             return getChampion("Champion").Spellbook.CanUseSpell(slot) == SpellState.Ready;
         }
 
-        private int getDangerLevelSkill(String champ, SpellSlot spell) {
+        private int getDangerLevel(String champ, SpellSlot spell) {
             if (champ == "Fiddlesticks" && spell == SpellSlot.Q)
                 return 1*getChampion(champ).Spellbook.GetSpell(spell).Level;
             return 5;
